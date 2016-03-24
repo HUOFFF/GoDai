c97987cdc8f276198abd47b97a22be7b48c5e08c
Hjunrong <1428996094@qq.com>
2016-03-25 02:45:41 +0800

    LoginActivity change 2nd

---
+++ app/src/main/java/demo/godai/LoginActivity.java
@@ -0,0 +1,20 @@
+package demo.godai;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Rong on 2016/3/25.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
