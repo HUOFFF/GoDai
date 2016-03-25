32d1f8816c8a7c08c4602f9c4382b3bc956daacb
Hjunrong <1428996094@qq.com>
2016-03-25 09:37:24 +0800

    LoginActivity change 3rd

--- app/src/main/java/demo/godai/LoginActivity.java
+++ app/src/main/java/demo/godai/LoginActivity.java
@@ -3,6 +3,7 @@ package demo.godai;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,5 +17,16 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK&&event.getRepeatCount() == 0){
+            Intent intent = new Intent();
+            intent.setClass(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            this.finish();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
--- app/src/main/java/demo/godai/MainActivity.java
+++ app/src/main/java/demo/godai/MainActivity.java
@@ -3,12 +3,14 @@ package demo.godai;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button bt_login;
+    private Button bt_register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,11 +20,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-                intent.setClass(MainActivity.this,LoginActivity.class);
+                intent.setClass(MainActivity.this, LoginActivity.class);
+                MainActivity.this.startActivity(intent);
+                MainActivity.this.finish();
+            }
+        });
+        bt_register = (Button) findViewById(R.id.bt_register);
+        bt_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, RegisterActivity.class);
                 MainActivity.this.startActivity(intent);
                 MainActivity.this.finish();
             }
         });
 
     }
+
 }
+
+
